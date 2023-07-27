61dc6211e082c35bf731eabfbaa9fc67f97feeb9
Jim Carter <jcarter62@gmail.com>
2023-07-27 10:25:38 -0700

    Add ALLOWED_HOSTS to .env and sample.env. This will help with deployment.

--- sgmaui/settings.py
+++ sgmaui/settings.py
@@ -27,8 +27,11 @@ SECRET_KEY = config('SECRET', default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+ALLOWED_HOSTS_CONFIG = config('ALLOWED_HOSTS', default='')
+if ALLOWED_HOSTS_CONFIG == '':
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+else:
+    ALLOWED_HOSTS = [ALLOWED_HOSTS_CONFIG]
 
 # Application definition
 
