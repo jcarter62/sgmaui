932d3bcb92197bafaab327bdca27336721ebac7f
Jim Carter <jcarter62@gmail.com>
2023-07-27 09:44:42 -0700

    Updated base.html to use cdn for jquery Updated STATICFILES_DIRS Removed dead code in auth/views.

--- sgmaui/settings.py
+++ sgmaui/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    BASE_DIR / '/static/',
+#    BASE_DIR / '/static/',
 ]
 
 # Default primary key field type
