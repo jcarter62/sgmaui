2f594912210f9e979142c5f93fa1cd6be66ffccc
Jim Carter <jcarter62@gmail.com>
2024-07-25 07:41:26 -0700

    Add CC Exclude code to the gwcalc input screen, and also add suggested values to the UI.

--- static/gwcalc_inputs.js
+++ static/gwcalc_inputs.js
@@ -27,7 +27,31 @@ function gwcalc_load_values() {
     load_value('gc_calc_date');
     load_value('gc_tc_code');
     load_value('gc_code_code');
+    load_value('gc_cc_exclude');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const divIds = ['code1', 'code2', 'code3'];
+
+    divIds.forEach(divId => {
+        const div = document.getElementById(divId);
+        const input = div.querySelector('input');
+        const small = div.querySelector('small');
+
+        if (input && small) {
+            small.classList.add('hidden');
+
+            input.addEventListener('focus', () => {
+                small.classList.remove('hidden');
+            });
+
+            input.addEventListener('blur', () => {
+                small.classList.add('hidden');
+            });
+        }
+    });
+});
+
 
 gwcalc_load_values();
 
